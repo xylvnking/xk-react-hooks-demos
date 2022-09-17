5ab6e383114ae0bfec8fde00984efe6a743e120d
Dylan King <xylvnking@gmail.com>
2022-09-17 12:43:44 -0400

    recreated app using typescript

---
+++ pages/Components/UseStateHook.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+export default function UseStateHook({}: Props) {
+  return (
+    <div>
+        <h1>this one demonstrates useState</h1>
+        <a target='_blank' href='https://reactjs.org/docs/hooks-reference.html#usestate'>documentation</a>
+    </div>
+  )
+}
