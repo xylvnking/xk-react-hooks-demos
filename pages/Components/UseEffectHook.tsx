e788123bdba7966ecd578203b536253c6aa70562
Dylan King <xylvnking@gmail.com>
2022-09-19 23:34:23 -0400

    sass is amazing

--- pages/Components/UseEffectHook.tsx
+++ pages/Components/UseEffectHook.tsx
@@ -18,49 +18,33 @@ export default function UseEffectHook() {
     },[valueInDependencyArray])
 
 
-    
-
+    // infinite loop hello world example
+    // const [state, setState] = useState(0);
+    // useEffect(() => {setState(state + 1)})
 
 
     return (
         <div className={hookStyles.container}>
-            <h1>use<span>Effect</span></h1>
-            <a target='_blank' href='https://reactjs.org/docs/hooks-reference.html#useeffect' className={hookStyles.link}>official documentation</a>
-            <br />
-            <a target='_blank' href='https://reactjs.org/docs/hooks-reference.html#useeffect' className={hookStyles.link}>source code</a>
-            <br />
-            <a target='_blank' href='https://reactjs.org/docs/hooks-reference.html#useeffect' className={hookStyles.link}>code sandbox</a>
-
-            <h2> {'>'} useEffect designates a 'side effect' of the initial render</h2>
-                <h3>This means that <strong>useEffect fires off the code within the callback function <em className={hookStyles.blueText}>after</em> the component is rendered</strong> && <strong>does <em className={hookStyles.blueText}>not</em> block browser painting.</strong></h3>
-                <p>useEffect is one of the most common React Hooks for good reason. We primarily use it to run blocks of code which are important to the application, but shouldn't block the initial page render.</p>
-                
-            <ul>
-                {/* <li>What this means is that useEffect fires off <em>after</em> the component is rendered and does <strong>not</strong> block browser painting.</li> */}
-                {/* <h4>Common use cases for useEffect:</h4> */}
-                <li className={hookStyles.listTitle}><strong>✅ Common use cases for useEffect:</strong></li>
-                <li>Fetching data from an external API</li>
-                <li>Sending analytics data to an external API</li>
-                <li>Directly updating the DOM</li>
-                <li>Timers</li>
-                
-                
-                {/* <li>What this means is that code inside of a useEffect can be made to run every re-render, only when the component initially loads (or in removed), or according to when a specific value changes.</li> */}
-                {/* <li>useEffect is the functional component replacement for componentDidMount and componentDidUpdate.</li> */}
-                {/* <li>useEffect is commonly causes problems for beginners who create inifinite loop by assing in a function which updates the piece of state on which the effect is dependent.</li> */}
-                {/* <li>useEffect takes a callback function to be executed, and a dependency array to dictate when that function is executed. If there is no array, the function is called on every render. If there is an empty array the function is called only once when the component is mounted. If there are any values within the array, the function will call when the component loads as well as whenever the value within the dependency array changes.</li> */}
-            </ul>
-            <ul>
-                <li className={hookStyles.listTitle}><strong>❌ Common mistakes when using useEffect:</strong></li>
-                <li><span className={hookStyles.redText}>Causing an infinite loop</span> by updating state within the callback function and <strong>forgetting to include a dependency array.</strong></li>
-                <li><span className={hookStyles.redText}>Causing an infinite loop</span> by updating state within the callback function and <strong>populating the dependency array with that same state.</strong></li>
-                <li><span className={hookStyles.redText}><em>Relying</em> on data fetched within the callback to create a user interface.</span> It's common to fetch data, store it in state, and then map over that state to create a dynamic user interface - but a check must be performed to <span className={hookStyles.blueText}>ensure the component doesn't break if the fetch within the useEffect callback doesn't deliver the expected data.</span> This is commonly done with a simple && conditional operator, or a ternary.</li>
-            </ul>
-                <small><em><strong>PS</strong> - useLayoutEffect is useEffect except it blocks the initial render, and is useful for visual elements like modals</em></small>
-            <hr />
-
-            <h2>useEffect <em>'hello world'</em>:</h2>
-            <SyntaxHighlighter language="javascript" style={nightOwl} className={hookStyles.syntax} customStyle={{padding:'0px', margin: '0px', fontSize: '14px'}}>
+            <header>
+                <h1><small>use</small><span>Effect</span></h1>
+                <nav>
+                    <a target='_blank' href='https://reactjs.org/docs/hooks-reference.html#useeffect' className={hookStyles.link}>official documentation</a>
+                    {/* <br /> */}
+                    <a target='_blank' href='https://reactjs.org/docs/hooks-reference.html#useeffect' className={hookStyles.link}>source code</a>
+                    {/* <br /> */}
+                    <a target='_blank' href='https://reactjs.org/docs/hooks-reference.html#useeffect' className={hookStyles.link}>code sandbox</a>
+                </nav>
+            </header>
+            <main>
+                <h2> {'>'} useEffect designates a 'side effect' of the initial render</h2>
+                    <h3>This means that <strong>useEffect fires off the code within the callback function <em className={hookStyles.blueText}>after</em> the component is rendered</strong> && <strong>does <em className={hookStyles.blueText}>not</em> block browser painting.</strong></h3>
+                    <section>
+                        <p>useEffect is one of the most common React Hooks. We primarily use it to run blocks of code which are important to the application, but shouldn't block the initial page render.</p>
+                        <p>It runs its provided <em className={hookStyles.blueText}>callback function</em> on every render by default, but by including a <em className={hookStyles.blueText}>dependency array</em> we are given control over this behaviour. Providing an empty dependency array as the second argument states that the effect doesn't use any value that participates in the React data flow.</p>
+                        <p>When we provide a value for the dependency array, we're stating that the effect is relevant to the data flow and that the callback should be fired off.</p>
+                    </section>
+<h2>useEffect <em>'hello world'</em>:</h2>
+                    <SyntaxHighlighter language="javascript" style={nightOwl} className={hookStyles.syntax} customStyle={{padding:'0px', margin: '0px', fontSize: '14px'}}>
 {`
     useEffect(() => {
         // do thing
@@ -71,6 +55,39 @@ export default function UseEffectHook() {
 
 `}
 </SyntaxHighlighter>
+                    <hr />
+                <ul>
+                    {/* <li>What this means is that useEffect fires off <em>after</em> the component is rendered and does <strong>not</strong> block browser painting.</li> */}
+                    {/* <h4>Common use cases for useEffect:</h4> */}
+                    {/* <li className={hookStyles.listTitle}><strong>✅ Common use cases for useEffect:</strong></li> */}
+                    <li className={hookStyles.listTitle}><strong>When to use useEffect:</strong></li>
+                    <li>Fetching data from an external API</li>
+                    <li>Sending analytics data to an external API</li>
+                    <li>When a component receives data via props and populates its own local state with it</li>
+                    <li>In most cases where you would have used <em className={hookStyles.blueText}>componentDidMount</em> or <em className={hookStyles.blueText}>componentDidUpdate</em></li>
+                    <li>Directly updating the DOM. </li>
+                    <li>Timers</li>
+                    
+                    
+                    {/* <li>What this means is that code inside of a useEffect can be made to run every re-render, only when the component initially loads (or in removed), or according to when a specific value changes.</li> */}
+                    {/* <li>useEffect is the functional component replacement for componentDidMount and componentDidUpdate.</li> */}
+                    {/* <li>useEffect is commonly causes problems for beginners who create inifinite loop by assing in a function which updates the piece of state on which the effect is dependent.</li> */}
+                    {/* <li>useEffect takes a callback function to be executed, and a dependency array to dictate when that function is executed. If there is no array, the function is called on every render. If there is an empty array the function is called only once when the component is mounted. If there are any values within the array, the function will call when the component loads as well as whenever the value within the dependency array changes.</li> */}
+                </ul>
+
+                <hr />
+
+                <ul>
+                    {/* <li className={hookStyles.listTitle}><strong>❌ Common mistakes when using useEffect:</strong></li> */}
+                    <li className={hookStyles.listTitle}><strong>Common mistakes:</strong></li>
+                    <li><span className={hookStyles.redText}>Causing an infinite loop</span> by updating state within the callback function and <strong>forgetting to include a dependency array.</strong></li>
+                    <li><span className={hookStyles.redText}>Causing an infinite loop</span> by updating state within the callback function and <strong>populating the dependency array with that same state.</strong></li>
+                    <li><span className={hookStyles.redText}><em>Relying</em> on data fetched within the callback to create a user interface.</span> It's common to fetch data, store it in state, and then map over that state to create a dynamic user interface - but a check must be performed to <span className={hookStyles.blueText}>ensure the component doesn't break if the fetch within the useEffect callback doesn't deliver the expected data.</span> This is commonly done with a simple && conditional operator, or a ternary.</li>
+                </ul>
+                    <small><em><strong>PS</strong> - useLayoutEffect is useEffect except it blocks the initial render, and is useful for visual elements like modals</em></small>
+            </main>
+            <hr />
+
 <section className={hookStyles.exampleSection}>
     <h2>A concrete example:</h2>
     <section>
